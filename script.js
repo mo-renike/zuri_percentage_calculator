3cb882a1207afa6110d51f4f75b302cad661eecb
Morenike Oyewole <herroyalpianist@gmail.com>
2021-05-26 11:42:29 +0100

    memes ear and dear

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
 function checkScore() {
     let num =  document.getElementById("score").value ;
   if (num > 25) {
-      let errorMessage = "Total score can not be more than 25";
+      let errorMessage = `jo, bawo lo se gba ${num} over 25?`;
       document.getElementById("display").innerText = errorMessage;
+      document.getElementById("3").style.display = "block";
       return errorMessage;
     }
     //if it is not a number
     else if (isNaN(num)){
-      let error = "Please enter a valid number"
+      let error = `So, You mean to tell me "${num}" a valid number`
       document.getElementById("display").innerText = error;
+      document.getElementById("1").style.display = "block";
         return error ;
     }
     else{
-      let result = (num / 25) * 100 + "%";
-      document.getElementById("display").innerText = result;
+      let result = (num / 25) * 100;
+      if (result >= 90) {
+        document.getElementById("5").style.display = "block";
+        document.getElementById("display").innerText = `${result}% osheyyy Mark Zuckerberg!`;
+        
+      } else if (result >= 80) {
+        document.getElementById("7").style.display = "block";
+        document.getElementById("display").innerText = `${result}%  You are doing well!`;
+        
+      } else if (result >= 65) {
+        document.getElementById("display").innerText = `${result}% Tor, You sef come`;
+        document.getElementById("2").style.display = "block";
+      } else if (result >= 50) {
+        document.getElementById("display").innerText = `${result}% Shey you will not drop out and be selling pure water bayi`;
+        document.getElementById("6").style.display = "block";
+      } else if (result >= 30 ) {
+        document.getElementById("display").innerText = `${result}% lol just get out of here`;
+        document.getElementById("4").style.display = "block";
+      } else if (result < 29 && result!== 0) {
+        document.getElementById("display").innerText = `${result}% boya you should give up dear`;
+        document.getElementById("8").style.display = "block";
+      }
+      
       return result;
-    }
+    } 
     
   }
   checkScore();
