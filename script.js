d7743b5bf2d74d1ab861e48697e00b75bcaa33c5
Morenike Oyewole <herroyalpianist@gmail.com>
2021-05-27 22:51:38 +0100

    .

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function checkScore() {
       document.getElementById("3").style.display = "block";
       return errorMessage;
     }
-    //if it is not a number
     else if (isNaN(num)){
       let error = `So, You mean to tell me "${num}" a valid number`
       document.getElementById("display").innerText = error;
@@ -17,28 +16,24 @@ function checkScore() {
       let result = ((num / 24) * 100).toFixed(2);
       if (result >= 90) {
         document.getElementById("5").style.display = "block";
-        document.getElementById("display").innerText = `${result}% osheyyy Mark Zuckerberg!`;
+        document.getElementById("display").innerText = `${result}% osheyyy Mark Zuckerberg Opr!`;
         
       } else if (result >= 80) {
         document.getElementById("7").style.display = "block";
         document.getElementById("display").innerText = `${result}%  You are doing well!`;
         
       } else if (result >= 65) {
-        document.getElementById("display").innerText = `${result}% Tor, You sef come`;
+        document.getElementById("display").innerText = `${result}% Tor, You tried your best`;
         document.getElementById("2").style.display = "block";
       } else if (result >= 50) {
         document.getElementById("display").innerText = `${result}% Shey you will not drop out and be selling pure water bayi`;
         document.getElementById("6").style.display = "block";
       } else if (result >= 30 ) {
+        document.getElementById("display").innerText = `${result}% I don commot my hand`;
+        document.getElementById("8").style.display = "block";
+      } else if (result < 29 && result > 0) {
         document.getElementById("display").innerText = `${result}% lol just get out of here`;
         document.getElementById("4").style.display = "block";
-      } else if (result < 29 && result!== 0) {
-        document.getElementById("display").innerText = `${result}% boya you should give up dear`;
-        document.getElementById("8").style.display = "block";
-      }
-      
-      return result;
+      }return result;
     } 
-    
-  }
-  checkScore();
+  }checkScore();
