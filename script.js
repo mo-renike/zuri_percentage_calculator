e8cd4bdfcc09774a71d5096ae285e16684987b33
Morenike Oyewole <herroyalpianist@gmail.com>
2021-05-28 11:30:09 +0100

    (-_-)

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function checkScore() {
       let errorMessage = `jo, bawo lo se gba ${num} over 24?`;
       document.getElementById("display").innerText = errorMessage;
       document.getElementById("3").style.display = "block";
-      //// setTimeout((clear) => {window.location.reload()}, 3000);
+      setTimeout(() => {window.location.reload()}, 3000);
       return errorMessage;
     }
     else if (isNaN(num)){
       let error = `So, You mean to tell me "${num}" a valid number`
       document.getElementById("display").innerText = error;
       document.getElementById("1").style.display = "block";
-      //// setTimeout((clear) => {window.location.reload()}, 3000);
+      setTimeout(() => {window.location.reload()}, 3000);
         return error ;
     }
     else{
@@ -19,33 +19,30 @@ function checkScore() {
       if (result >= 90) {
         document.getElementById("5").style.display = "block";
         document.getElementById("display").innerText = `${result}% osheyyy Mark Zuckerberg Opor!`;
-        // setTimeout(() => {window.location.reload()}, 3000);
+        setTimeout(() => {window.location.reload()}, 3000);
 
         
       } else if (result >= 80) {
         document.getElementById("7").style.display = "block";
         document.getElementById("display").innerText = `${result}%  You are doing well!`;
-        // setTimeout(() => {window.location.reload()}, 3000);
+        setTimeout(() => {window.location.reload()}, 3000);
         
       } else if (result >= 65) {
         document.getElementById("display").innerText = `${result}% Tor, You tried your best`;
         document.getElementById("2").style.display = "block";
-        // setTimeout(() => {window.location.reload()}, 3000);
+        setTimeout(() => {window.location.reload()}, 3000);
       } else if (result >= 50) {
         document.getElementById("display").innerText = `${result}% Shey you will not drop out and be selling pure water bayi`;
         document.getElementById("6").style.display = "block";
-       // setTimeout((clear) => {window.location.reload()}, 3000);
+       setTimeout(() => {window.location.reload()}, 3000);
       } else if (result >= 30 ) {
         document.getElementById("display").innerText = `${result}% I don commot my hand`;
         document.getElementById("8").style.display = "block";
-       // setTimeout((clear) => {window.location.reload()}, 3000);
+       setTimeout(() => {window.location.reload()}, 3000);
       } else if (result < 29 && result > 0) {
         document.getElementById("display").innerText = `${result}% lol just get out of here`;
         document.getElementById("4").style.display = "block";
-       // setTimeout((clear) => {window.location.reload()}, 3000);
-      }
-      if (num.value.length > 0){
-        setTimeout(() => {window.location.reload()}, 3000);
+       setTimeout(() => {window.location.reload()}, 3000);
       }
      
     }  return result;
